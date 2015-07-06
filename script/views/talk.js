3f44cce264bf19f21b08b649d7a70e8cfe82411e
Maxim Zakharov <mszakharov@gmail.com>
2015-07-06 11:29:13 +0000

    Update version to 3

--- script/views/talk.js
+++ script/views/talk.js
@@ -358,7 +358,7 @@
 (function() {
 
     var notice;
-    var version = 2;
+    var version = 3;
 
     function showNotice() {
         notice = $('<div class="updated-notice"><div class="updated-text">Вышло обновление Talkrooms. Пожалуйста, <span class="updated-reload">обновите страницу</span>, чтобы сбросить кэш браузера.</div></div>')
