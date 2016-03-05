c7f224973610604ae5c055a5333b6d44b253e11f
Maxim Zakharov <mszakharov@gmail.com>
2016-03-05 13:40:56 +0000

    Update version to 30

--- script/models/main.js
+++ script/models/main.js
@@ -88,7 +88,7 @@ var Me = {};
 (function() {
 
     var notice;
-    var version = 29;
+    var version = 30;
 
     function showNotice(description) {
         notice = $('<div class="updated-notice"></div>')
