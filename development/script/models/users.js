afbead0c8ad4e5d58e82f7047edc6c75ecc196c6
Maxim Zakharov <mszakharov@gmail.com>
2017-04-16 08:22:01 +0000

    Make roles sorting case-insensitive

--- development/script/models/users.js
+++ development/script/models/users.js
@@ -3,6 +3,28 @@
 
     var extend = Object.assign || $.extend;
 
+    // Compare function for sorting
+    function byAlias(a, b) {
+        if (a.alias > b.alias) return  1;
+        if (a.alias < b.alias) return -1;
+        return 0;
+    }
+
+    // Normalize nickname for case-insensitive sorting
+    function setAlias(role) {
+        role.alias = role.nickname.toLowerCase();
+    }
+
+    // Insert role into sorted array and return inserted position
+    function insertRole(list, role) {
+        for (var i = list.length; i--;) {
+            if (role.alias < list[i].alias) {
+                list.splice(i, 0, role);
+                return i;
+            }
+        }
+    }
+
     // Remove role from array
     function removeRole(list, role) {
         for (var i = list.length; i--;) {
@@ -12,16 +34,6 @@
         }
     }
 
-    // Insert role to array sorted by nickname
-    // and return inserted position
-    function insertRole(list, role) {
-        for (var i = list.length; i--;) {
-            if (role.nickname < list[i].nickname) {
-                list.splice(i, 0, role);
-                return i;
-            }
-        }
-    }
 
     // Collection constructor
     function Roles() {
@@ -38,6 +50,8 @@
         reset: function(roles) {
             this.index = {};
             this.items = [];
+            roles.forEach(setAlias);
+            roles.sort(byAlias);
             for (var i = roles.length; i--;) {
                 var role = roles[i];
                 this.index[role.role_id] = role;
@@ -49,9 +63,10 @@
             if (this.index[data.role_id]) {
                 this.update(data);
             } else {
+                setAlias(data);
                 insertRole(this.items, data);
                 this.index[data.role_id] = data;
-                this.trigger('added', data);
+                //this.trigger('added', data);
             }
         },
 
@@ -60,7 +75,7 @@
             if (role) {
                 removeRole(this.items, role);
                 delete this.index[roleId];
-                this.trigger('removed', role);
+                //this.trigger('removed', role);
             }
         },
 
@@ -68,7 +83,12 @@
             var role = this.index[data.role_id];
             if (role) {
                 extend(role, data);
-                this.trigger('updated', role);
+                if (data.nickname) {
+                    setAlias(data);
+                    removeRole(this.items, role); // Reinsert role to
+                    insertRole(this.items, role); // keep items sorted
+                }
+                //this.trigger('updated', role);
             }
         },
 
